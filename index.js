6de3f0fe4b42e089d4730a8400aea5a80353f6b5
Baskar <baymax1298@gmail.com>
2017-12-14 11:59:07 +0530

    added signup

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var express = require("express"),
-    app     = express(),
+var express           = require("express"),
+    app               = express(),
     ejs               = require("ejs"),
     passport          = require("passport"),
     plm               = require("passport-local-mongoose"),
     mongoose          = require("mongoose"),
-    LocalStrategy     = require("passport-local");
+    LocalStrategy     = require("passport-local"),
+    bodyParser        = require("body-parser");
 
 
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var DB_URL= "mongodb://localhost/12thapp";
 
@@ -46,6 +48,11 @@ UserSchema.plugin(plm);
 var User = mongoose.model("User", UserSchema);
 
 // SERIALIZE AND DESERIALIZE USER
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -61,47 +68,28 @@ app.get("/register",function(req,res){
 })
 
 // // post routes
-// app.post("/register",function(req,res){
-//
-//var fullname = req.body.name,
-//     email    = req.body.email,
-//     number   = req.body.number,
-//     password = req.body.password;
-//
-//
-//   User.register(new User({
-//       username: fullname,
-//       email: email,
-//       phonenumber: number
-//       }),
-//       req.body.password,function(err,user){
-//       passport.authenticate("local")(req,res,function(){
-//           console.log(user);
-//           // req.flash('success','You are Registered and now can LogIn');
-//           res.redirect("/login");
-// });
-//
+app.post("/register",function(req,res){
 
-app.post("/register", function(req,res){
-var name =req.body.fullname;
+var  username = req.body.username,
+     email    = req.body.email,
+     number   = req.body.number,
+     password = req.body.password;
+
+
    User.register(new User({
-     username: name,
-     email: req.body.email
-   }), req.body.password, function(err, newid){
-        if(err){
-            console.log(err);
-            return res.send(err);
-         }
-        passport.authenticate("local")(req, res, function(){
-            res.render("/login");
-            console.log("newid");
-
-        });
-
-
-    })
-
+       username: username,
+       email: email,
+       phonenumber: number
+      }),
+      req.body.password,function(err,user){
+      passport.authenticate("local")(req,res,function(){
+         console.log(user);
+           // req.flash('success','You are Registered and now can LogIn');
+          res.redirect("/");
 });
+});
+});
+
 
 
 app.listen(8080,()=>{
